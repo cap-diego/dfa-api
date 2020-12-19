320f6a98b8fff6c5a9ff22d5b4262961167f21f1
Diego <diegobuceta35@gmail.com>
2020-12-19 12:00:35 -0300

    Agregado endpoint para minimzar automata

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cap-diego/dfa-api
 
 go 1.15
+
+require github.com/cap-diego/dfa-minimization-algorithm v0.0.0-20201214013457-6cbd518af77c
--- main.go
+++ main.go
@@ -3,22 +3,37 @@ package main
 
 import (
 	"time"
-	"context"
-	"os/signal"
 	"os"
 	"fmt"
+	"context"
+	"os/signal"
 	"net/http"
-	dfa "github.com/cap-diego/dfa-minimization-algorithm"
+	"encoding/json"
+	"github.com/cap-diego/dfa-minimization-algorithm"
 )
 
 
 func minimizeAutomata(w http.ResponseWriter, r *http.Request) {
-	
+	var M dfa.DFA
+	if r.Method != http.MethodPost {
+		http.Error(w, "error, method post expected", 400)
+		return
+	}
+	if r.Body == nil {
+		http.Error(w, "error, body expected", 400)
+		return
+	}
+	err := json.NewDecoder(r.Body).Decode(&M)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error while decoding dfa, %s\n", err.Error()), 404)
+		return
+	}
+	Min := dfa.HopcroftDFAMin(M)
+	json.NewEncoder(w).Encode(Min)
 }
 
 func main() {
 	fmt.Print("Inicializando server\n")
-	fmt.Printf("%#v\n", dfa)
 	http.HandleFunc("/minimize", minimizeAutomata)
 	go func() {
 		err := http.ListenAndServe(":8080", nil)
@@ -33,5 +48,5 @@ func main() {
 
 	sig := <- sigChan // Block 
 	fmt.Print("Terminate: ", sig)
-    context.WithTimeout(context.Background(), 30 * time.Second)
+    context.WithTimeout(context.Background(), 10 * time.Second)
 }
